a67e71f365717abd48237aedbab8988974b494ec
EstherOA <estherowusuansah19@gmail.com>
2021-09-16 00:27:16 +0000

    linked chat and chatlist to firebase

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,17 +1,141 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react'
+import {View, Text, StyleSheet} from 'react-native';
+import { Bubble, GiftedChat, Send } from 'react-native-gifted-chat'
+import Icon from 'react-native-vector-icons/Ionicons';
+import { colors } from '@theme/colors';
+import AvatarComponent from 'src/components/avatar/AvatarComponent';
+import { firebase } from 'src/firebase/config';
 
 const ChatScreen = (props) => {
 
+    const [messages, setMessages] = useState([]);
+    const [chatInfo, setChatInfo] = useState();
+    const mainForumRef = firebase.firestore().collection('GFELTPforum');
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, []);
+
+  const navigateChatList = () => {
+      props.navigation.navigate('ChatList');
+  }
+
+  const saveToFirebase = (messages) => {
+      console.log(messages);
+      mainForumRef.collection('messages')
+      .add(messages)
+      .then( doc => {
+          console.log(doc);
+      })
+      .catch(error => {
+          console.warn("Failed to save message to firebase:", error);
+      })
+  }
+
+  const fetchForumMessages = () => {
+      mainForumRef.collection('messages').orderBy('createdAt', 'desc')
+      .onSnapshot( 
+          querySnapshot => {
+              let newMessages = [];
+              querySnapshot.forEach(doc => {
+                  newMessages.push[{...doc.data(), _id: doc.id}]
+              });
+              setMessages(newMessages);
+          },
+          error => {
+              console.warn("Failed to fetch forum messages:", error);
+          }
+      );
+  }
+
+  const handleSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
+    saveToFirebase(messages);
+  }, []);
+
+  const renderSend = (props) => {
+      return (
+          <Send {...props}>
+              <Icon name="send" color={colors.lightPrimary} size={24} style={{marginBottom: 10, marginRight: 10}}/>
+          </Send>
+      );
+  }
+
+  const renderBubble = (props) => {
+      return (
+            <Bubble {...props}
+                wrapperStyle={{
+                    left: {backgroundColor: colors.white},
+                    right: {backgroundColor: colors.lightPrimary}
+                }}        
+            />
+      )
+  }
+
     return (
-        <View>
-            <Text>Chat</Text>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.iconButtonView}>
+                    <Icon name="arrow-back-sharp" size={24} color={colors.white} onPress={navigateChatList}/>
+                </View>
+                <AvatarComponent src={require('@assets/logo_1.jpg')} avatarContainerStyle={styles.avatar}/>
+                <View>
+                    <Text style={styles.chatName}>GFELTP Forum</Text>
+                </View>
+            </View>
+            <GiftedChat
+            messages={messages}
+            renderUsernameOnMessage 
+            alwaysShowSend
+            scrollToBottom
+            renderSend={renderSend}
+            renderBubble={renderBubble}
+            onSend={handleSend}
+            user={{
+                _id: 1,
+            }}
+            />
         </View>
-    );
+    )
 }
 
 export default ChatScreen;
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+    },
+    header: {
+        flexDirection: 'row',
+        paddingHorizontal: 20,
+        height: 50,
+        alignItems: 'center',
+        backgroundColor: colors.lightPrimary
+    },
+    iconButtonView: {
+        marginRight: 20
+    },
+    avatar: {
+        width: 50, 
+        height: 50, 
+        borderRadius: 25,
+        marginRight: 30
+    },
+    chatName: {
+        fontWeight: '600',
+        fontSize: 15,
+        color: colors.white,
+        textAlign: 'center'
+    }
 });
--- src/screens/ChatList.js
+++ src/screens/ChatList.js
@@ -3,10 +3,12 @@ import { Text, View, StyleSheet, FlatList, Pressable } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import AvatarComponent from '@components/avatar/AvatarComponent';
 import { colors } from 'src/theme/colors';
+import { safeConvertToString } from 'src/utils/helperFunctions';
 
 const ChatListScreen = (props) => {
 
     const [chatList, setChatList] = useState();
+    const mainForumRef = firebase.firestore().collection('GFELTPforum');
 
     useEffect(() => {
         (async () => {
@@ -14,45 +16,62 @@ const ChatListScreen = (props) => {
                 {
                     id: 1,
                     name: 'GFELTP Forum',
-                    members: [
-                        {
-                            id: 0,
-                            firstname: '',
-                            surname: ''
-                        }
-                    ],
+                    avatar: '',
                     latest_message: {
                         time: '10:47',
                         text: 'Hello all',
                         user: {
-                            firstname: 'John',
+                            name: 'John K.',
                             id: 0
                         }
                     },
                 }
-            ])
+            ]);
+            fetchForumInfo();
         })();
     }, []);
 
-    const navigateChatScreen = () => {
-        props.navigation.navigate('ChatScreen');
+    const fetchForumInfo = () => {
+        mainForumRef.onSnapshot( querySnapshot => {
+            let chatInfo = {};
+            querySnapshot.forEach(doc => {
+                chatInfo = {...doc.data(), id: doc.id};
+            });
+            setChatList([...chatList, chatInfo]);
+        }, 
+        error => {
+            console.warn("Error fetching forum details:", error)
+        })
+    }
+
+    const navigateChatScreen = (chatInfo) => {
+        props.navigation.navigate('ChatScreen', {
+            chatInfo
+        });
+    }
+
+    const navigateSettings = () => {
+        props.navigation.navigate('ManageUser');
     }
 
     const renderItem = ({item}) => {
         return (
-            <Pressable style={styles.listContainer} onPress={navigateChatScreen}>
+            <Pressable style={styles.listContainer} onPress={() => navigateChatScreen(item)}>
                 <View>
                     <AvatarComponent avatarContainerStyle={styles.avatar}/>
                 </View>
                 <View style={styles.centerView}>
                     <View style={styles.centerHeader}>
                     <Text style={styles.chatName}>{item.name}</Text>
-                    <Text style={styles.messageTime}>{item.latest_message.time}</Text>
+                    <Text style={styles.messageTime}>{safeConvertToString(item.latest_message.time)}</Text>
                     </View>
-                    <View style={styles.messageView}>
-                    <Text style={styles.latestSender}>{item.latest_message.user.firstname}:</Text>
-                    <Text> {item.latest_message.text}</Text>
-                    </View>
+                    {
+                        item.latest_message &&
+                        <View style={styles.messageView}>
+                            <Text style={styles.latestSender}>{item.latest_message.user.firstname}:</Text>
+                            <Text> {item.latest_message.text}</Text>
+                        </View>
+                    }
                 </View>
             </Pressable>
         )
@@ -61,6 +80,9 @@ const ChatListScreen = (props) => {
     return (
         <View style={styles.mainContainer}>
             <Text style={styles.mainHeaderText}>Conversations</Text>
+            <View>
+                <AvatarComponent avatarContainerStyle={styles.userAvatar} onPress={navigateSettings}/>
+            </View>
             <FlatList renderItem={renderItem} keyExtractor={(item) => item.id.toString()} data={chatList}/>
         </View>
     );
@@ -110,5 +132,10 @@ const styles = StyleSheet.create({
         fontWeight: '600',
         textTransform: 'uppercase',
         marginBottom: 20
-    }
+    },
+    userAvatar: {
+        width: 40,
+        height: 40,
+        borderRadius: 20
+    },
 });
